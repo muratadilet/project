616d95c113b4c0eae3c9caf6571c2606c2739052
Adilet <adilet.mb01@gmail.com>
2024-01-28 11:58:53 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number); 
+
+// const obj = {
+//     a: 50
+// };
+// obj.a = 10;
+// console.log(obj);
+
+// "use sctrict";
+// let number = 4.6;
+
+// console.log(4/0);
+//   console.log('string' * 9);
+//   const person = "Alex";
+
+// const obj = {
+//     name: "Adilet",
+//     age: 22,
+//     isMarried: false
+// };
+
+// // console.log(obj.name);
+// console.log(obj["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp',];
+"use strict";
+// const result = confirm("Are you here?");
+// console.log(result);
+
+const answer = prompt("Вам есть 18?", "18");
+console.log(answer);
